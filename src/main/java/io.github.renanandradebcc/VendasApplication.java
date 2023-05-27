264b8377882e53c070d01024ba41755a98f17e5e
Renan Andrade <contatorenanandradebcc@gmail.com>
2023-05-27 14:47:06 -0300

    Conhecendo a anotação ComponentScan.

--- src/main/java/io.github.renanandradebcc/VendasApplication.java
+++ src/main/java/io.github.renanandradebcc/VendasApplication.java
@@ -4,10 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+
+// Não é obrigatorio usar o ComponentScan para fazer ele avistar repositorio ou service,
+// ele serve mais para casos de mapeamento de lib externas para que o metodo possa ter acesso
+// a um env ou algo relacionado ao retorno de um metodo externo
+@ComponentScan(basePackages = {
+        "io.github.renanandradebcc.repository",
+        "io.github.renanandradebcc.service"}
+)
 @RestController
 public class VendasApplication {
 
---
+++ src/main/java/io.github.renanandradebcc/repository/MyRepository.java
@@ -0,0 +1,8 @@
+package io.github.renanandradebcc.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MyRepository {
+
+}
